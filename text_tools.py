ccf23b73b205532bef7e83fbdb34f5545adc1e4a
Brian Hu <brian.h.hu@gmail.com>
2025-10-08 13:20:58 -0400

    imported sentiment analysis, calculated pmi

--- text_tools.py
+++ text_tools.py
@@ -1,6 +1,43 @@
 from collections import Counter
 import nltk
 from nltk.tokenize import word_tokenize
+from gensim.models import Word2Vec
+import numpy as np
+
+
+def embedding(corpus):
+    sentences = []
+    for uttr in corpus:
+        tokens = word_tokenize(uttr.lower())
+        sentence = []
+        for tok in tokens:
+            if tok.isalpha():
+                sentence.append(tok)
+        sentences.append(sentence)
+    return Word2Vec(sentences=sentences, vector_size=100, window=10, sg=1)
+
+def classifier_pmi(corpus, id_terms, labels, num_classes):
+    N = len(corpus)
+    id_count = 0
+    labels_pair_count = np.zeros(num_classes)
+    labels_single_count = np.zeros(num_classes)
+
+    for i, uttr in enumerate(corpus):
+        label = labels[i]  #np.array 
+        labels_single_count += label
+        for id_term in id_terms:
+            if id_term in word_tokenize(uttr.lower()):
+                id_count += 1
+                labels_pair_count += label
+                break 
+    
+    pmis = (np.log2(N * labels_pair_count) 
+            - np.log2(id_count * labels_single_count)
+            ).tolist()
+    return pmis
+
+        
+
 
 def process(corpus, stopwords):
     """
